7af5bd80bff474bc270dfb90cdd1311e8bab0e37
AlexiaCazale <alexiacazale7@gmail.com>
2024-10-25 10:43:52 -0300

    Criando dynamic routes - seleção dos animais da list-render e exibição deles em uma nova página

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -22,4 +22,8 @@ export class ListService {
   getAll(): Observable<Animal[]> {
     return this.http.get<Animal[]>(this.apiUrl);
   }
+
+  getItem(id: number): Observable<Animal>{
+    return this.http.get<Animal>(`${this.apiUrl}/${id}`);
+  }
 }
